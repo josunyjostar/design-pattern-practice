3dc2e9a14ae6b1e6aac2e09959c54857c5ca5cd0
박제영 <jypark@newploy.net>
2023-10-20 17:46:46 +0900

    feat : strategy parttren 1차

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,8 @@ const path = require('path');
 module.exports = {
     mode: 'development',
     //entry: "./src/composite/index.ts",
-    entry: './src/adapter/index.ts',
+    //entry: './src/adapter/index.ts',
+    entry: './src/strategy/index.ts',
     devtool: 'inline-source-map',
     module: {
         rules: [
